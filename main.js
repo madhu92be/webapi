7b160a7d53688d3262354a7f1ba3d305fdb751cf
Madhu <madhu92be@gmail.com>
2016-06-13 10:03:10 +0530

    events concept completed

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 var fs = require('fs');
+// data = fs.readFileSync('input.txt');
+// console.log(data.toString());
 fs.readFile('input.txt',function (err,data) {
   if(err) return console.error(err);
   console.log(data.toString());
 });
+console.log('Program ended');
+
+
+var events = require('events');
+
+eventEmitter = new events.EventEmitter();
+
+var startedHandler = function started() {
+  // body...
+  console.log('program started');
+}
+
+eventEmitter.on('started',startedHandler);
+
+var eventHandler = function connected() {
+  console.log('connection connected');
+  eventEmitter.emit('started');
+}
+
+eventEmitter.on('connection',eventHandler);
+
+eventEmitter.emit('connection');
+
 
